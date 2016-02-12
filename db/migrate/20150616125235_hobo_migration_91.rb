1a5786aa7b9a4ab6776023f76742ea7f31ff92d6
Txinto Vaz <txinto@gatatac.org>
2016-02-12 03:26:20 +0100

    Fix to avoid problems with 64 char limit on sqlite

--- db/migrate/20150616125235_hobo_migration_91.rb
+++ db/migrate/20150616125235_hobo_migration_91.rb
@@ -7,9 +7,10 @@ class HoboMigration91 < ActiveRecord::Migration
     add_column :uds_services, :generate, :boolean, :default => true
 
     remove_index(:uds_service_fixed_params, [:configuration_switch_id])
+    remove_index(:uds_service_fixed_params, [:uds_sub_service_id])
     change_column :uds_service_fixed_params, :generate, :boolean, :default => true
     add_index(:uds_service_fixed_params, [:configuration_switch_id], :name => "add_index_to_fixparam_cfgswtch")
-    
+    add_index(:uds_service_fixed_params, [:uds_sub_service_id], :name => "add_index_to_fixparam_subserv")
   
   end
 
@@ -21,7 +22,9 @@ class HoboMigration91 < ActiveRecord::Migration
     remove_column :uds_services, :generate
 
     remove_index(:uds_service_fixed_params, :name => "add_index_to_fixparam_cfgswtch")
+    remove_index(:uds_service_fixed_params, :name => "add_index_to_fixparam_subserv")
     change_column :uds_service_fixed_params, :generate, :boolean
     add_index(:uds_service_fixed_params, [:configuration_switch_id])
+    add_index(:uds_service_fixed_params, [:uds_sub_service_id])
   end
 end
